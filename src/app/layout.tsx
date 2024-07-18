476d1f8b39e3fb3af7d1b697228eb8d6a4d5d801
capricornus44 <antonzavalniuk@gmail.com>
2024-07-18 23:42:30 +0200

    chore: install required dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import {Inter} from 'next/font/google'
 
 import type {Metadata} from 'next'
 import './globals.scss'
+import {cn} from '@/lib/utils'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen antialiased', inter.className)}>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
-  const a = 25
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-2xl font-semibold'>Home page</h1>
+    <main>
+      <h1>Home page</h1>
     </main>
   )
 }
